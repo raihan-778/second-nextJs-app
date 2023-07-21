d752912d7cddda487238fddcd6eae19b18686abb
raihan-778 <raihanarif778@gmail.com>
2023-07-21 17:32:39 +0600

    nested ,dynamic & catch all route added

---
+++ src/pages/news/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CatchAllRoutes = () => {
+  return (
+    <div>
+      <h1>This page is for filtration</h1>
+    </div>
+  );
+};
+
+export default CatchAllRoutes;
---
+++ src/pages/news/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsHomePage = () => {
+  return (
+    <div>
+      <h1>This is news home page</h1>
+    </div>
+  );
+};
+
+export default NewsHomePage;
