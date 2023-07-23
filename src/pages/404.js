606d6f315a44f21921c0457e3b39fb23753a5195
raihan-778 <raihanarif778@gmail.com>
2023-07-23 10:08:53 +0600

    ant design added

---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <div>
+        {" "}
+        <img
+          src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000"
+          alt=""
+          width="100%"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
